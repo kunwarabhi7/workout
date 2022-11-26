73938df2a9f5fb8f0368b573211f319ff22d15d5
Abhishek <abhishekparmar162@gmail.com>
2022-11-26 13:21:27 +0530

    added navigation to navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Logo from '../public/assests/logo.png'
+import Link from 'next/link'
 const NavBar = () => {
   return (
     <div className='flex justify-between bg-gray-500 text-white'>
-      
+      <Link href='/'>
 <Image src={Logo} className='w-12 pt-4 pl-2' />
+      </Link>
 <h1 className='text-white font-bold pt-6 text-xl pr-3'>Be FIt With US.</h1>        
     </div>
   )
